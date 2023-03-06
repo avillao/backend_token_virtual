d2dc7fd30bc583cec19b1949df7f9b637ade6d4f
Aarón Villao <villaoaron@gmail.com>
2023-03-06 15:54:21 -0500

    Documentaci

--- server/api/migrations/0002_insert_data.py
+++ server/api/migrations/0002_insert_data.py
@@ -3,6 +3,7 @@ from django.contrib.auth.management import create_permissions
 from django.apps import apps
 from os import error
 from django.contrib.auth.hashers import make_password
+from django.conf import settings
 
 def create_applications_permissions():
     try:
@@ -10,11 +11,11 @@ def create_applications_permissions():
             create_permissions(app_config)
     except(LookupError, ImportError) as e:
         raise error("%s. Are you sure your INSTALLED_APPS setting is correct?" % e)
-
-
+    
 def insert_data(apps, schema_migration):
     create_applications_permissions()
     User = apps.get_model('auth', 'User')
+    Transaction = apps.get_model('api', 'Transaction')
 
     user1 = User(id=1,username="user1",email='user1@example.com')
     user1.password = make_password("Prueba_123")
@@ -28,11 +29,16 @@ def insert_data(apps, schema_migration):
     user3.password = make_password("Prueba_123")
     user3.save()
 
+    t1 = Transaction.objects.create(name = "Cambio de contraseña")
+    t2 = Transaction.objects.create(name = "Acceso a la banca web")
+    t3 = Transaction.objects.create(name = "Acceso desde la banca móvil" )
+
 
 class Migration(migrations.Migration):
         
     dependencies = [
-        ('api','0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('api','0001_initial'),('auth','0006_require_contenttypes_0002')
     ]
 
     operations = [
